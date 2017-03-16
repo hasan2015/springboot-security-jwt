1710475d2a49bece213bbf85fc3037a31b7097c8
yanhaixun <yanhaixun@360ser.com>
2017-03-16 20:23:38 +0800

    curl执行成功。下一步，继续看 http://blog.csdn.net/a82793510/article/details/53509427 然后研究另一个 http://www.jdon.com/dl/best/json-web-tokens-spring-cloud-microservices.html 对比看security如何jwt

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into APP_USER(ID, PASSWORD, USERNAME) values(1, '$2a$10$bnC26zz//2cavYoSCrlHdecWF8tkGfPodlHcYwlACBBwJvcEf0p2G', 'svlada@gmail.com');
 insert into USER_ROLE(APP_USER_ID, ROLE) values(1, 'ADMIN');
 insert into USER_ROLE(APP_USER_ID, ROLE) values(1, 'PREMIUM_MEMBER');
+
+--curl -X POST -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json" -H "Cache-Control: no-cache" -d "{"""username""": """svlada@gmail.com""","""password""":"""test1234"""}" "http://localhost:9966/api/auth/login"
+--curl -X GET -H "X-Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdmxhZGFAZ21haWwuY29tIiwic2NvcGVzIjpbIlJPTEVfQURNSU4iLCJST0xFX1BSRU1JVU1fTUVNQkVSIl0sImlzcyI6Imh0dHA6Ly9zdmxhZGEuY29tIiwiaWF0IjoxNDg5NjY2MzE4LCJleHAiOjE0ODk2NjcyMTh9.pBltxzF1WLFtGFG8pYQgfNC2YcOKh0GsLQbf4fbqHmDsBFqEGuk8yyhkSOdJxZrKP8KriQ3Xt-anaGWSq3_Odg" -H "Cache-Control: no-cache" "http://localhost:9966/api/me"
